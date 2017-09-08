9abb2dfe46b057d15d08ce587755a5a95db17029
空格书签 <jiangkuan@163.com>
2017-09-08 13:57:53 +0800

    JeeSite 项目

---
+++ src/main/webapp/static/p2pProj/js/AjaxUtils.js
@@ -0,0 +1,21 @@
+var AjaxUtils = {
+    //基础选项
+    option: {
+        method: "get",//默认提交的方法,get post
+        url: "",//请求的路径 required
+        param: {},//请求的参数
+        type: "text",//返回的内容的类型,text,xml,json
+        callback: function () {
+            //回调函数 required
+        }
+    },
+    //创建XMLHttpRequete对象
+    createRequest: function () {
+        var xmlhttp;
+        try {
+
+        } catch (e) {
+
+        }
+    }
+};
